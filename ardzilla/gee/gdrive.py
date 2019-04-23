2fe45d2d7bcc7cf42e166000ae328e6b3bbcd2f4
Chris Holden <ceholden@gmail.com>
2019-04-23 12:04:40 -0400

    Implement mkdir_p & fix exists for duplicates

--- ardzilla/gee/gdrive.py
+++ ardzilla/gee/gdrive.py
@@ -9,7 +9,7 @@ MIME_TYPE_DIRECTORY = 'application/vnd.google-apps.folder'
 MIME_TYPE_FILE = 'application/vnd.google-apps.file'
 
 
-def mkdir_p(service, dest):
+def mkdir_p(service, dest, parent_id=None):
     """ Make a directory, recursively
 
     Parameters
@@ -24,19 +24,32 @@ def mkdir_p(service, dest):
     str
         Directory created
     """
-    paths = dest.split('/')
-    # TODO
+    paths = dest.split('/', 1)
+    if len(paths) == 1:  # root
+        name_id = exists(service, paths[0], parent_id=parent_id)
+        if not name_id:
+            return mkdir(service, paths[0], parent_id=parent_id)
+        else:  # already exists
+            return name_id
+    else:
+        name, paths_ = paths[0], paths[1:]
+        name_id = exists(service, name, parent_id=parent_id, directory=True)
+        if not name_id:
+            name_id = mkdir(service, name, parent_id=parent_id)
+        dest_ = '/'.join(paths_)
+        return mkdir_p(service, dest_, parent_id=name_id)
 
 
-def mkdir(service, path, parent_id=None):
+def mkdir(service, name, parent_id=None):
     """ Make a directory on GDrive
     """
     meta = {
-        'name': path,
+        'name': name,
         'mimeType': MIME_TYPE_DIRECTORY,
     }
     if parent_id is not None:
         meta['parents'] = [parent_id]
+    logger.debug(f'Creating directory {name}')
     dir_ = service.files().create(body=meta, fields='id').execute()
     return dir_['id']
 
@@ -67,7 +80,7 @@ def exists(service, name, parent_id=None, directory=False, trashed=False):
         f'name = "{name}"'
     ]
     if directory:
-        q.append('mimeType = "{MIME_TYPE_DIRECTORY}"')
+        q.append(f'mimeType = "{MIME_TYPE_DIRECTORY}"')
     if parent_id is not None:
         q.append(f'"{parent_id}" in parents')
 
@@ -75,7 +88,9 @@ def exists(service, name, parent_id=None, directory=False, trashed=False):
     logger.debug(f'Searching for query: "{q_}"')
     query = service.files().list(q=q_).execute().get('files', [])
 
-    if len(query) == 1:
+    if query:
+        if len(query) > 1:
+            logger.debug('Found more than 1 result -- returning first')
         return query[0]['id']
     else:
         return ''
