e9b59fec3db4b8e56459b875e8c773c123976f8e
Chris Holden <ceholden@gmail.com>
2019-04-24 16:24:57 -0400

    Add docs

--- ardzilla/gee/core.py
+++ ardzilla/gee/core.py
@@ -32,6 +32,11 @@ def export_name(collection, tile, d_start, d_end,
         Starting period
     date_end : dt.datetime
         Ending period
+
+    Returns
+    -------
+    str
+        Name for exported data
     """
     # We need a LOT of metadata in the name...
     collection_ = collection.replace('/', '-')
@@ -45,6 +50,24 @@ def export_name(collection, tile, d_start, d_end,
 
 def export_path(collection, tile, d_start, d_end,
                 version='v01', prefix='GEEARD'):
+    """ Generate a prefix/folder path for some data
+
+    Parameters
+    ----------
+    imgcol : str
+        GEE image collection name
+    tile : stems.gis.grids.Tile
+        STEMS TileGrid tile
+    date_start : dt.datetime
+        Starting period
+    date_end : dt.datetime
+        Ending period
+
+    Returns
+    -------
+    str
+        Name for prefix/folder path to exported data
+    """
     collection_ = collection.replace('/', '-')
     hv = f"h{tile.horizontal:03d}v{tile.vertical:03d}"
 
@@ -62,27 +85,43 @@ def submit_ard(collection, tile, date_start, date_end, store):
 
     Parameters
     ----------
-    TODO...
+    collection : str
+        GEE image collection name
+    tile : stems.gis.grids.Tile
+        STEMS TileGrid tile
+    date_start : dt.datetime
+        Starting period
+    date_end : dt.datetime
+        Ending period
+    store : gcs.GCSStore or gdrive.GDriveStore
+        Data store helper object from this package
 
     Returns
     -------
-    ?
+    ee.batch.Task
+        Earth Engine task
+    str
+        Metadata object store information (object ID if using the GDrive
+        store or a path if using GCS)
     """
     assert isinstance(store, (gcs.GCSStore, gdrive.GDriveStore))
     logger.debug(f'Storing image and metadata using {store}')
+
     # TODO: Better place/way of getting this... just outsourcing for now...
     logger.debug(f'Submitting ARD for collection {collection} between '
                  f'{date_start}-{date_end}')
     name = export_name(collection, tile, date_start, date_end)
     path = export_path(collection, tile, date_start, date_end)
 
-    # TODO: switch on collection to call into appropriate module
+    # Determine which function should be used for ARD generation
     func_create_ard = CREATE_ARD_COLLECTION[collection]
     logger.debug(f'Using function {func_create_ard}')
 
+    # Actually create it...
     logger.debug('Creating ARD...')
     image, metadata = func_create_ard(collection, tile, date_start, date_end)
 
+    # Stores are agnostic about these keywords needed for image
     logger.debug('Creating Task to calculate and store image...')
     kwds = {
         'crs': tile.crs.wkt,
@@ -91,9 +130,9 @@ def submit_ard(collection, tile, date_start, date_end, store):
     task = store.store_image(image, name, path, **kwds)
 
     logger.debug('Storing metadata...')
-    metadata_id = store.store_metadata(metadata, name, path)
+    metadata_ = store.store_metadata(metadata, name, path)
 
-    return task, metadata_id
+    return task, metadata_
 
 
 # TODO: add below
