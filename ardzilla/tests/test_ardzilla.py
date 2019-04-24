c3e8c977c41cb2d4109edc2f2ca66c1836c6c046
Chris Holden <ceholden@gmail.com>
2019-04-24 13:12:38 -0400

    Add fake test so pytest exits 0

--- ardzilla/tests/test_ardzilla.py
+++ ardzilla/tests/test_ardzilla.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pytest
+
+
+# Dummy test so pytest doesn't exit with return code 5 (no tests ran)
+def test_package():
+    import ardzilla
+    assert ardzilla.__version__
