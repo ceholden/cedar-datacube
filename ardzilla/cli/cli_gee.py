9399912e1a7537e47af7aabd27ab7e8840737e7a
Chris Holden <ceholden@gmail.com>
2019-05-15 14:01:40 -0400

    Add barebones login cli programs

--- ardzilla/cli/cli_gee.py
+++ ardzilla/cli/cli_gee.py
@@ -1,12 +1,12 @@
 """ CLI for GEE pre-ARD submissions and downloads
 """
+import logging
 from pkg_resources import iter_entry_points
 
 import click
 from click_plugins import with_plugins
 
 from . import options
-
 
 # Help links
 DOCS_INSTALL_GEE = 'https://developers.google.com/earth-engine/python_install_manual'
@@ -27,19 +27,22 @@ def group_login(ctx):
     pass
 
 
-@group_login.command('ee', help='Log in to GEE services')
+@group_login.command('ee', help='Test logging into to GEE service')
 @click.pass_context
 def login_gee(ctx):
-    """ Login to the Google Earth Engine
+    """ Test logging into the Google Earth Engine
+
+    If it doesn't work, please make sure you've authenticated by running
+    >>> earthengine authenticate
     """
     try:
         import ee
     except ImportError:
         msg = ('Could not load Earth Engine Python API. Please install '
                '`earthengine` and try again. '
-               'More information at {DOCS_INSTALL_GEE}')
+               'More information at:\n{DOCS_INSTALL_GEE}')
         click.echo(options.STYLE_ERROR(msg))
-        click.exit(1)
+        raise click.Abort()
 
     try:
         ee.Initialize()
@@ -48,20 +51,33 @@ def login_gee(ctx):
                f'are authenticated. More information at:\n{DOCS_AUTH_GEE}')
         click.echo(options.STYLE_ERROR(str(e)))
         click.echo(options.STYLE_ERROR(msg))
+        raise click.Abort()
     else:
         click.echo(options.STYLE_INFO('Authenticated'))
-        click.exit(1)
+
+
+@group_login.command('gdrive', help='Login to use Google Drive')
+@click.option('--client_secrets_file', help='OAuth2 "client secrets" file',
+              type=options.type_path_file)
+@click.option('--credentials_file', help='OAuth2 credentials',
+              type=options.type_path_file)
+@options.opt_no_browser
+@click.pass_context
+def login_gdrive(ctx, client_secrets_file, credentials_file, no_browser):
+    """ Login to the Google Drive API service using OAuth2 credentials
+    """
+    # TODO: try to build gdrive service
+    from ardzilla.gee import gdrive
+    creds = gdrive.get_credentials(client_secrets_file=client_secrets_file,
+                                   credentials_file=credentials_file,
+                                   no_browser=no_browser)
+    service = gdrive.build_gdrive_service(creds)
+    click.echo(options.STYLE_INFO('Logged in'))
 
 
 @group_login.command('gcs', help='Login to use Google Cloud Storage')
 @click.pass_context
 def login_gcs(ctx):
     # TODO: try to build gcs service
-    from ardzilla.gee.gauth import build_gcs_client
-
-
-@group_login.command('gdrive', help='Login to use Google Drive')
-@click.pass_context
-def login_gdrive(ctx):
-    # TODO: try to build gdrive service
-    from ardzilla.gee.gauth import build_gdrive_service
+    from ardzilla.gee import gcs
+    raise NotImplementedError("TODO")
--- ardzilla/cli/options.py
+++ ardzilla/cli/options.py
@@ -15,3 +15,10 @@ STYLE_ERROR = _click_style(None, 'red')
 STYLE_WARNING = _click_style(None, 'red')
 STYLE_INFO = _click_style(None, None)
 STYLE_DEBUG = _click_style(None, 'cyan')
+
+# Types
+type_path_file = click.Path(exists=True, dir_okay=False, resolve_path=True)
+
+
+# Options
+opt_no_browser = click.option('--no-browser', is_flag=True, help='Do not launch a web browser')
