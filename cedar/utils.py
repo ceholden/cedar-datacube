f6a6a9e796217e4c9446a39f838d2eb7748fe7e8
Chris Holden <ceholden@gmail.com>
2019-06-20 10:31:08 -0400

    Create util for importing EE api and handling errors

--- cedar/utils.py
+++ cedar/utils.py
@@ -56,3 +56,55 @@ def affine_to_str(transform):
     """ Return a string representatin of an affine.Affine transform
     """
     return ','.join(map(str, transform[:6]))
+
+
+def load_ee(initialize=True):
+    """ Import and initialize the EE API, handling errors
+
+    Parameters
+    ----------
+    initialize : bool, optional
+        Try to initialize the EE API, or not
+
+    Returns
+    -------
+    object
+        Earth Engine API module ``ee``
+
+    Raises
+    ------
+    ImportError
+        Raised if the Earth Engine cannot be imported
+    ee.ee_exceptions.EEException
+        Raised if the Earth Engine cannot be initialized, typically because
+        you haven't authenticated yet.
+    Exception
+        Passes other exceptions caused by :py:func:`ee.Initialize`
+    """
+    try:
+        import ee as ee_api
+    except ImportError as ie:
+        docs = 'https://developers.google.com/earth-engine/python_install'
+        pip_info = '`pip` ("pip install earthengine-api")'
+        conda_info = '`conda` ("conda install -c conda-forge earthengine-api")'
+        raise ImportError(
+            f'Cannot import the Earth Engine API. Please install the package, '
+            f'which is available through {pip_info} or {conda_info}. Visit '
+            f'"{docs}" for more information.'
+        )
+    else:
+        if initialize:
+            try:
+                ee_api.Initialize()
+            except ee_api.ee_exception.EEException as eee:
+                raise eee
+            except Exception:
+                logger.exception(
+                    'Could not initialize EarthEngine API. Depending on the '
+                    'error, this might be caused by network access issues.'
+                )
+                raise
+            else:
+                return ee_api
+        else:
+            return ee_api
