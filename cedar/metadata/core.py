6e501ee43a52f7b131e807e76c6994412800716f
Chris Holden <ceholden@gmail.com>
2019-07-10 16:51:10 -0400

    Reorganize tracking metadata reprs

--- cedar/metadata/core.py
+++ cedar/metadata/core.py
@@ -1,10 +1,11 @@
 """ Classes and other core tools for tracking & image metadata
 """
-from collections import Mapping
+from collections import Mapping, defaultdict
 import datetime as dt
 import json
 import os
-import time
+
+import numpy as np
 
 
 _HERE = os.path.abspath(os.path.dirname(__file__))
@@ -60,207 +61,173 @@ class TrackingMetadata(Mapping):
 
     # repr, printing, etc info
     def __repr__(self):
-        # TODO
-        return "CEDAR Tracking Metadata"
+        return repr_tracking(self)
 
     def __html_repr__(self):
         # TODO
         return "CEDAR Tracking Metadata"
 
 
-def get_order_statistics(orders_info):
-    orders_stats = dict()
-
-    orders_stats['order_amount'] = len(orders_info)
-    completed = 0
-    unsubmitted = 0
-    runtime_list = []
-    active = 0
-    for order in orders_info:
-        print(order)
-        if order['status']['state'] == 'COMPLETED':
-            completed += 1
-        elif order['status']['state'] == 'UNSUBMITTED':
-            runtime = check_runtime(order)
-            if runtime != -1:
-                runtime_list.append(runtime)
-            unsubmitted += 1
-        else:
-            runtime = check_runtime(order)
-            if runtime != -1:
-                runtime_list.append(runtime)
-            active += 1
-    orders_stats['completed_amount'] = completed
-    orders_stats['unsubmitted_amount'] = unsubmitted
-    orders_stats['active_amount'] = active
-    if len(runtime_list) != 0:
-        orders_stats['avg_runtime'] = sum(runtime_list) / len(runtime_list)
-    orders_stats['avg_runtime'] = -1
-    return orders_stats
+def repr_metadata_program(info):
+    lines = [
+        f'* {info["name"]}={info["version"]}',
+        f'* earthengine-api={info["ee"]}'
+    ]
+    return wrapping_indent(lines, 'Program Info:')
 
 
-def check_runtime(order):
-    start_time = order['status']['start_timestamp_ms']
-    update_time = order['status']['update_timestamp_ms']
-    now_timestamp = int(round(time.time()))
-    if start_time != '' and update_time != '':
-        runtime = (update_time - start_time)
-    elif start_time != '' and update_time == '':
-        runtime = (now_timestamp - start_time)
-    else:
-        runtime = -1
-    return runtime
+def repr_tracking_submission(info):
+    tile_indices = [f'({i[0]}, {i[1]})' for i in info['tile_indices']]
+
+    period_info = [
+        f'* Start: {info["period_start"]}',
+        f'* End:   {info["period_end"]}',
+        f'* Freq:  {info["period_freq"]}'
+    ]
+    period_info_str = wrapping_indent(period_info, '* Period: ', 8)
+
+    lines = [
+        f'* Submitted on {info["submitted"]}',
+        f'* Tile Grid: "{info["tile_grid"]["name"]}"',
+        f'* Tile Indices : {", ".join(tile_indices)}',
+        period_info_str
+    ]
+    return wrapping_indent(lines, 'Submission Info:')
 
 
-def convert_datetime_to_print(datetime_string):
-    timestamp = 'NaN'
-    if not (datetime_string == 0 or datetime_string is ''):
-        timestamp = dt.datetime.fromisoformat(datetime_string).strftime('%Y-%m-%d %H:%M:%S')
-    return timestamp
+def repr_tracking_tracking(info):
+    lines = [
+        f'* Name: {info["name"]}',
+        f'* Prefix: {info["prefix"]}',
+        f'* Collections: {", ".join(info["collections"])}',
+        f'* Image template: {info["name_template"]}',
+        f'* Image prefix: {info["prefix_template"]}'
+    ]
+    return wrapping_indent(lines, 'Tracking Info:')
 
 
-def print_selected_tiles(submission_info):
-    selected_tiles = submission_info['tile_indices']
-    output_str = ''
-    for i in range(len(selected_tiles)):
-        if i != len(selected_tiles) - 1:
-            output_str += f'    * {i + 1}: {selected_tiles[i][0]}, {selected_tiles[i][1]}\n'
-        else:
-            output_str += f'    * {i + 1}: {selected_tiles[i][0]}, {selected_tiles[i][1]}'
-    return output_str
+def repr_tracking_orders(info, show_states=True, show_runtimes=True):
+    summary = summarize_orders(info)
+
+    lines = [
+        f'* Count: {len(info)}'
+    ]
+    if show_states:
+        lines_ = [f'- {state}: {n}' for state, n in summary['states'].items()]
+        lines.extend(wrapping_indent(lines_, '* States:').splitlines())
+    if show_runtimes:
+        s_mean, s_std = [_format_runtime(summary['runtimes'][k])
+                         for k in ('mean', 'std', )]
+        line = f'* Runtime: {s_mean} +/- {s_std} minutes'
+        lines.append(line)
+    return wrapping_indent(lines, 'Orders:')
 
 
-def output_url_set(orders_info):
-    output_url = set()
-    for order in orders_info:
-        output_url.update(order['status']['output_url'])
-    return list(output_url)
-##
+def repr_metadata_order(info, header='Order:'):
+    summary = summarize_order(info)
+    s_runtime = _format_runtime(summary['runtime'])
+    lines = [
+        f'- Name: {summary["name"]}',
+        f'- Prefix: {summary["prefix"]}',
+        f'- ID: {summary["id"]}',
+        f'- Task state: {summary["state"]}',
+        f'- Runtime: {s_runtime} minutes',
+        f'- Output URL: {summary["output_url"]}',
+    ]
+    return wrapping_indent(lines, header)
 
 
-# cli printing implementations
-
-# cedar status info --program
-def info_program(program_info):
-    info = f'Ordered with {program_info["name"]} = {program_info["version"]} and Earth Engine = {program_info["ee"]}'
-    output = [info]
-    return output
-
-
-# cedar status info --submission
-def info_submission(submission_info):
-    submission_date = f'Submitted on {convert_datetime_to_print(submission_info["submitted"])}'
-    name = f'Tile Name: {submission_info["tile_grid"]["name"]}'
-    tile_range = f'Tile Indices:\n{print_selected_tiles(submission_info)}'
-    start = f'Start Period: {submission_info["period_start"]}'
-    end = f'End Period: {submission_info["period_end"]}'
-    freq = f'Period Frequency: {submission_info["period_freq"]}'
-    output = [submission_date, name, tile_range, start, end, freq]
-    return output
-
-
-# cedar status info --orders
-def info_orders(orders_info):
-    orders_stats = get_order_statistics(orders_info)
-    total = f'Total Orders: {orders_stats["order_amount"]}'
-    process = f'    Active: {orders_stats["active_amount"]}'
-    process += f'\n    Unsubmitted: {orders_stats["unsubmitted_amount"]}'
-    process += f'\n    Complete: {orders_stats["completed_amount"]}'
-    output = [total, process]
-    return output
-
-
-def info_orders_specific(orders_info):
-    orders_stats = get_order_statistics(orders_info)
-    order_output = []
-    avg_runtime = orders_stats['avg_runtime']
-    if avg_runtime != -1:
-        avg_runtime_out = f'Average Runtime: {avg_runtime} ms'
-    else:
-        avg_runtime_out = f'Average Runtime: NaN'
-    output_urls = f'Output URLs: {output_url_set(orders_info)}'
-    for count in range(1, orders_stats['order_amount']+1):
-        order_output += info_order(orders_info, count)
-    return [avg_runtime_out, output_urls] + order_output
-
-
-def info_order(orders_info, order_indice):
-    orders_stats = get_order_statistics(orders_info)
-    if orders_stats['order_amount'] < order_indice or 0 >= order_indice:
-        error_msg = 'Index out of range: Please enter a valid indice'
-        return [error_msg]
-    order = orders_info[order_indice-1]
-    order_name = f'Order Name: {order["name"]}'
-    order_id = f'    * ID: {order["status"]["id"]}'
-    order_state = f'    * State: {order["status"]["state"]}'
-
-    creation_time = order["status"]["creation_timestamp_ms"]
-    start_time = order["status"]["start_timestamp_ms"]
-    last_update_time = order["status"]["update_timestamp_ms"]
-
-    creation_timestamp = convert_datetime_to_print(creation_time)
-    start_timestamp = convert_datetime_to_print(start_time)
-    last_update_timestamp = convert_datetime_to_print(last_update_time)
-
-    order_created = f'    * Created: {creation_timestamp}'
-    order_start = f'    * Started: {start_timestamp}'
-    order_last_update = f'    * Last Updated: {last_update_timestamp}'
-    output = [order_name, order_id, order_state, order_created, order_start, order_last_update]
-    return output
-
-
-# cedar status info <tracking_id> --program --submission --orders --order <order_indices>
-def info_status(tracking_data, show_program_info=True, show_submission_info=True, show_orders_info=True,
-                show_order_specific=True, show_order=True, order_indice=3):
+def repr_tracking(tracking_data,
+                  show_program=True, show_submission=True, show_tracking=True,
+                  show_states=True, show_runtimes=True,
+                  show_orders=None):
+    """ Return string formatted information about CEDAR tracking metadata
     """
-    A function for cli command, 'cedar status info ...'
+    lines = []
 
-    Parameters
-    ----------
-    tracking_data : dict
-        Required argument which specifies which tracking file information to be displayed
-    show_program_info : bool, optional
-        Optional argument with default value of True. Specified by the '--program' flag in CLI.
-        Displays program information.
-    show_submission_info : bool, optional
-        Optional argument with default value of True. Specified by the '--submission' flag in CLI.
-        Displays submission information.
-    show_orders_info : bool, optional
-        Optional argument with default value of True. Specified by the '--program' flag in CLI.
-        Displays status of orders in a submission
-    show_order_specific : bool, optional
-        Optional argument with default value of False. Specified by the '--program' flag in CLI.
-        Displays specific information about individual orders in a submission.
-    show_order : bool, optional
-        Optional argument with default value of False. Specified by the '--order <order_indice>' flag in CLI.
-        order_indice must be specified when this flag is raised.
-        Shows specific information on a single order specified
-    order_indices : int, optional
-        Optional argument with default value of -1. Specified by the '--program' flag in CLI.
-        Used as an argument for show_order flag to specify which order to be displayed
+    if show_program:
+        lines.append(repr_metadata_program(tracking_data['program']))
+    if show_submission:
+        lines.append(repr_tracking_submission(tracking_data['submission']))
+    if show_tracking:
+        lines.append(repr_tracking_tracking(tracking_data['tracking']))
 
-    Returns
-    -------
-    output : list
-        Outputs a list of strings. Each element in the list represents a line in CLI.
+    # Top level order information is always added
+    lines.append(repr_tracking_orders(tracking_data['orders'],
+                                      show_states=show_states,
+                                      show_runtimes=show_runtimes))
+
+    if show_orders is not None:
+        # Handle 'True', which is shortcut to showing all order details 
+        if show_orders is True:
+            show_orders = range(len(tracking_data['orders']))
+
+        # Otherwise expect sequence of order indexes (1st, 2nd, etc)
+        for order_id in show_orders:
+            order_md = tracking_data['orders'][order_id]
+            order_repr = repr_metadata_order(order_md, f'Order #{order_id}')
+            lines.append(order_repr)
+
+    return '\n'.join(lines)
+
+
+# =============================================================================
+def calculate_order_runtime(start_time, update_time, nan=None):
+    if start_time and update_time:
+        return update_time - start_time
+    elif start_time:
+        now = dt.datetime.now().timestamp()
+        return now - start_time
+    else:
+        return nan
+
+
+def summarize_order(order):
+    summary = {k: order[k] for k in ('name', 'prefix', )}
+    summary['id'] = order['status']['id']
+    summary['state'] = order['status']['state']
+    summary['runtime'] = calculate_order_runtime(
+        order['status']['start_timestamp_ms'],
+        order['status']['update_timestamp_ms'])
+    summary['output_url'] = list(set(order['status']['output_url']))
+
+    return summary
+
+
+def summarize_orders(orders):
+    """ Return summaries and stats (counts by state & runtimes) of all orders
     """
-    output = []
+    summaries = [summarize_order(o) for o in orders]
 
-    if show_program_info:
-        program_info = get_status_program_info(tracking_data)
-        output += info_program(program_info)
+    order_states = defaultdict(lambda: 0)
+    for summary in summaries:
+        order_states[summary['state']] += 1
+    order_states = dict(order_states)
 
-    if show_submission_info:
-        submission_info = get_status_submission_info(tracking_data)
-        output += info_submission(submission_info)
+    runtimes = np.array([
+        summary['runtime'] for summary in summaries
+        if summary['runtime'] is not None
+    ])
 
-    orders_info = get_status_orders_info(tracking_data)
-    if show_orders_info or show_order_specific:
-        output += info_orders(orders_info)
-        if show_order_specific:
-            output += info_orders_specific(orders_info)
+    return {
+        'summaries': summaries,
+        'states': order_states,
+        'runtimes': {
+            'total': runtimes.sum(),
+            'n': runtimes.size,
+            'mean': np.mean(runtimes),
+            'std': np.std(runtimes)
+        }
+    }
 
-    if show_order and order_indice != -1:
-        output += info_order(orders_info, order_indice)
 
-    return output
+def wrapping_indent(lines, heading='', length=4):
+    indent = '\n' + ' ' * length
+    if heading:
+        lines = [heading] + lines
+    return indent.join(lines)
+
+
+
+def _format_runtime(time_ms):
+    return '{0:02.2f}'.format(time_ms / 60 / 1000)
