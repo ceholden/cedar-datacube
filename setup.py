38477986358def6559343123164f74608791d676
Chris Holden <ceholden@gmail.com>
2019-07-19 13:18:48 -0400

    Update package pypi classifiers

--- setup.py
+++ setup.py
@@ -17,17 +17,16 @@ AUTHOR_EMAIL = 'ceholden@gmail.com'
 URL = 'https://github.com/ceholden/cedar-datacube'
 DESCRIPTION = "Creator for Analysis Ready Data (ARD)"
 CLASSIFIERS = [
-    'Development Status :: 2 - Pre-Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
     'Natural Language :: English',
-    "Programming Language :: Python :: 2",
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Scientific/Engineering :: GIS",
 ]
 
 with open('README.rst') as f:
