62dff70fd2cf798b0b2322c2802dc6c448636a0a
Chris Holden <ceholden@gmail.com>
2019-07-19 12:38:58 -0400

    Init conftest with links to vcrpy docs

---
+++ cedar/tests/conftest.py
@@ -0,0 +1,8 @@
+import json
+
+import pytest
+
+
+# import vcrpy
+# https://vcrpy.readthedocs.io/en/latest/advanced.html#filter-sensitive-data-from-the-request
+# https://vcrpy.readthedocs.io/en/latest/advanced.html#automatic-cassette-naming
